c8c642aa0def0ea548af6ede062f723e5fe8d3a1
Marc Tremblay <marcandre.tr@gmail.com>
2014-09-15 23:12:30 -0400

    Completed: map parser

--- src/hw1/uqam_map_parser.clj
+++ src/hw1/uqam_map_parser.clj
@@ -7,12 +7,23 @@
 (defn parse-part-1
   [line-to-parse node-map]
   (let [x (clojure.string/split (clojure.string/replace line-to-parse #"[(),]" "") #" ")]
-    (assoc node-map (atom (x 0))
+    (assoc node-map (keyword (x 0))
            {:geo [(Float/parseFloat (x 1)) (Float/parseFloat (x 2))]})))
 
 (defn parse-part-2
   [line-to-parse node-map]
-  node-map)
+  (let [x (clojure.string/split (clojure.string/replace line-to-parse #"[;:]" "") #"\s+")]
+    (loop [ret-map node-map elements (rest x)]
+      (if (= (count elements) 1)
+        ret-map
+        (recur (let [target-node (ret-map (keyword (first elements)))
+                     kw-to-add (keyword (second elements))]
+                 (assoc ret-map
+                   (keyword (first elements))
+                   (if (target-node :dest)
+                    (assoc target-node :dest (cons kw-to-add (target-node :dest)))
+                    (assoc target-node :dest [kw-to-add]))))
+               (rest elements))))))
 
 (defn parse-map
   "Parses file and outputs the node structure"
@@ -26,8 +37,6 @@
           (recur (rest rdr) 2 node-map)
           (if (= 1 file-part)
            (recur (rest rdr) 1 (parse-part-1 (first rdr) node-map))
-           (recur (rest rdr) 2 (parse-part-2 (first rdr) node-map))
-
-           )))))
+           (recur (rest rdr) 2 (parse-part-2 (first rdr) node-map)))))))
 
 )
