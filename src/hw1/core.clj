281238c9f0be74e9b8d284f4919f4595cdcff929
Marc Tremblay <marcandre.tr@gmail.com>
2014-09-22 19:52:03 -0400

    Generification of A* State generation New state structure

--- src/hw1/core.clj
+++ src/hw1/core.clj
@@ -17,20 +17,10 @@
                 :n8
                 heurist)]
       (println (format "# Elapsed time: %s ms" (- (System/currentTimeMillis) start-time)))
-      ans
-      ))
+      ans))
 
-  (let [heurist hw1.uqam_map_parser/cost-of-move
-        start-time (System/currentTimeMillis)]
 
-    (let [ans (hw1.allstar/a-star
-                (hw1.uqam_map_parser/parse-map "resources/uqam-map-1.txt")
-                :n1
-                :n8
-                heurist)]
-      (println (format "# Elapsed time: %s ms" (- (System/currentTimeMillis) start-time)))
-      ans
-      ))
+
 
   ;(def omap (hw1.osm_map_parser/parse-map "resources/carte-osm-simple.osm"))
 
--- src/hw1/uqam_map_parser.clj
+++ src/hw1/uqam_map_parser.clj
@@ -1,31 +1,34 @@
 (ns hw1.uqam_map_parser
   (:require [clojure.java.io :as io]
+            [swiss.arrows :refer :all]
             clojure.string)
   (:gen-class))
 
 (defn cost-of-move
-  [from to]
-  (let [[from-lng from-lat] (from :geo)
-        [to-lng to-lat] (to :geo)
-        rad-deg-ratio (/ (Math/PI) 180)
-        twice-earth-radius 12742000]
+  [world from to]
+  (let [from (world from)
+        to (world to)]
+    (let [[from-lng from-lat] (from :geo)
+          [to-lng to-lat] (to :geo)
+          rad-deg-ratio (/ (Math/PI) 180)
+          twice-earth-radius 12742000]
 
-    (let [lng1 (* from-lng rad-deg-ratio)
-          lng2 (* to-lng rad-deg-ratio)
-          lat1 (* from-lat rad-deg-ratio)
-          lat2 (* to-lat rad-deg-ratio)]
-      (let [s2 (Math/sin (/ (- lat2 lat1) 2))
-            s1 (Math/sin (/ (- lng2 lng1) 2))]
-        (*
-          twice-earth-radius
-          (Math/asin
-            (Math/sqrt
-              (+
-                (Math/pow s1 2)
-                (*
-                  (Math/cos lat1)
-                  (Math/cos lat2)
-                  (Math/pow s2 2))))))))))
+      (let [lng1 (* from-lng rad-deg-ratio)
+            lng2 (* to-lng rad-deg-ratio)
+            lat1 (* from-lat rad-deg-ratio)
+            lat2 (* to-lat rad-deg-ratio)]
+        (let [s2 (Math/sin (/ (- lat2 lat1) 2))
+              s1 (Math/sin (/ (- lng2 lng1) 2))]
+          (*
+            twice-earth-radius
+            (Math/asin
+              (Math/sqrt
+                (+
+                  (Math/pow s1 2)
+                  (*
+                    (Math/cos lat1)
+                    (Math/cos lat2)
+                    (Math/pow s2 2)))))))))))
 
 
 (defn parse-part-1
@@ -72,3 +75,56 @@
               (recur (rest rdr) 2 int-nodes-map (inc ways) (+ segments acc-segs))))))))
 
   )
+
+(defn- generate-one-state
+  [child-key child-f child-g parent-key opened closed states]
+  (cond (or
+          (and
+            (opened child-key)
+            (> ((opened child-key) :f) child-f))
+          (not (closed child-key))
+          )
+        [(assoc opened child-key child-f)
+         closed
+         (assoc states child-key {:g child-g :parent parent-key})]
+
+        (and (closed child-key) (> ((closed child-key) :f) child-f))
+        [(assoc opened child-key child-f)
+         (dissoc closed child-key)
+         (assoc states child-key {:g child-g :parent parent-key})]
+
+        :else
+        [opened closed states]))
+
+(defn generate-new-states
+  "Finds the new childrens and their cost"
+  [world states parent-state-key opened closed goal]
+
+  (loop [possible-destinations (-<> (parent-state-key :position) ((world <>) :dest))
+         new-opened opened
+         new-closed closed
+         new-states states]
+    (if (empty? possible-destinations)
+      [new-opened new-closed new-states]
+
+      (let [current-child-g (+ ((states parent-state-key) :g)
+                               (cost-of-move world parent-state-key (first possible-destinations)))]
+        (let [[ret-opened ret-closed ret-states]
+                (generate-one-state (first possible-destinations)
+                                    (+ current-child-g (cost-of-move world (first possible-destinations) goal))
+                                    current-child-g
+                                    parent-state-key
+                                    new-opened
+                                    new-closed
+                                    new-states)]
+
+          (recur
+            (rest possible-destinations)
+            ret-opened
+            ret-closed
+            ret-states
+            )))
+
+      )
+
+    ))
