b4b8c22552c2388d311889862a6927da24cca769
Marc-Antoine Sauvé <marc.antoine.sauve@gmail.com>
2014-09-22 21:23:39 -0400

    Calculate the time of A* only Map parsing optimisation

--- src/hw1/core.clj
+++ src/hw1/core.clj
@@ -8,29 +8,30 @@
   "I don't do a whole lot ... yet."
   [start-kw end-kw]
 
-  (let [heurist hw1.uqam_map_parser/cost-of-move
-        start-time (System/currentTimeMillis)]
+  (let [map (hw1.uqam_map_parser/parse-map "resources/uqam-map-1.txt")]
+    (let [heurist hw1.uqam_map_parser/cost-of-move
+          start-time (System/currentTimeMillis)]
 
-    (let [ans (hw1.allstar/a-star
-                (hw1.uqam_map_parser/parse-map "resources/uqam-map-1.txt")
-                :n1
-                :n8
-                heurist)]
-      (println (format "# Elapsed time: %s ms" (- (System/currentTimeMillis) start-time)))
-      ans
-      ))
+      (let [ans (hw1.allstar/a-star
+                  map
+                  :n1
+                  :n8
+                  heurist)]
+        (println (format "# Elapsed time: %s ms" (- (System/currentTimeMillis) start-time)))
+        ans
+        ))
 
-  (let [heurist hw1.uqam_map_parser/cost-of-move
-        start-time (System/currentTimeMillis)]
+    (let [heurist hw1.uqam_map_parser/cost-of-move
+          start-time (System/currentTimeMillis)]
 
-    (let [ans (hw1.allstar/a-star
-                (hw1.uqam_map_parser/parse-map "resources/uqam-map-1.txt")
-                :n1
-                :n8
-                heurist)]
-      (println (format "# Elapsed time: %s ms" (- (System/currentTimeMillis) start-time)))
-      ans
-      ))
+      (let [ans (hw1.allstar/a-star
+                  map
+                  :n1
+                  :n8
+                  heurist)]
+        (println (format "# Elapsed time: %s ms" (- (System/currentTimeMillis) start-time)))
+        ans
+        )))
 
   ;(def omap (hw1.osm_map_parser/parse-map "resources/carte-osm-simple.osm"))
 
